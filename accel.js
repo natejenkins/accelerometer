837b129bb519f18e15f13643de5368ab5e3693c0
Nate Jenkins <nate.jenkins@gmail.com>
2015-07-06 14:42:43 +0200

    realtime

--- accel.js
+++ accel.js
@@ -3,26 +3,47 @@ function vecLength(v){
   return Math.pow(lSquared, 0.5);
 }
 
+function makeArray(length, defaultValue){
+  var array = new Array(length);
+  for(i=0; i<length; i++){
+    array[i] = defaultValue;
+  }
+  return array;
+}
+
 $(function() {
-  var dataY = [];
-  var dataT = [];
-  var dataTY = [[0,1], [1,2], [2,3]];
-  totalPoints = 300;
 
-  var updateInterval = 30;
+  var dataAX = makeArray(length, 0);
+  var dataAY = makeArray(length, 0);
+  var dataAZ = makeArray(length, 0);
+  var dataT  = makeArray(length, 0);
+  var dataTAX = [[0,1], [1,3], [2,3]];
+  var dataTAY = [[0,1], [1,4], [2,3]];
+  var dataTAZ = [[0,1], [1,2], [2,3]];
+  var startTime = new Date().getTime();
+  totalPoints = 1000;
 
-  var plot = $.plot("#acceleration-plot", [dataTY], {
+  var updateInterval = 500;
+
+  var plot = $.plot("#acceleration-plot", [dataTAX, dataTAY, dataTAZ], {
     series: {
-      shadowSize: 0 // Drawing is faster without shadows
+      shadowSize: 0
     },
     yaxis: {
       min: -15,
       max: 15
+    },
+    xaxis: {
+      min: 0,
+      max: 100
     }
   });
 
   function update() {
-    plot.setData([dataTY]);
+    var time = (new Date().getTime() - startTime)/1000.0;
+    dataTAX = dataTAX.slice(1);
+    dataTAX.push([time, 5]);
+    plot.setData([dataTAX, dataTAY, dataTAZ]);
     plot.draw();
     setTimeout(update, updateInterval);
   }
